d609f5f12b91e98d97f8faed8acaea08c0f61f64
ReshmaJR <reshmajr1015@gmail.com>
2025-02-20 10:54:25 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,82 @@
+// ex 1
+ console.log("Hello World")
+
+ // ex 2
+ console.log(" Name : Reshma")
+
+ // ex 3
+ console.log("Name:Reshma \n Age:20")
+
+
+ // ex 4
+ let boolean=true;
+ let string="Hey";
+ let number=25
+ console.log(string+number)
+ console.log(string+"Siri")
+ console.log(number+5)
+ console.log(number+"5")
+
+// ex 5
+console.log("Reshma" + "20")
+
+// ex 6
+let name="Reshma"
+let age=20
+console.log("Name :" , name)
+console.log("Age :" , age)
+
+// ex 7
+let x=10
+let y=7
+console.log("Addition :" , (x+y)) 
+console.log("Subtraction :" , (x-y)) 
+console.log("Multiplication :" , (x*y)) 
+console.log("Division :" , (x/y))
+console.log("modulus :" , (x%y)) 
+
+// ex 8
+let arr=['a','b','c']
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+
+// ex 9
+
+let pattern = "";
+for(let i=1;i<=4;i++){
+    for(let j=1;j<=i;j++){
+        pattern +="*";
+    }   
+    pattern += "\n";
+}  
+console.log(pattern);
+
+// ex 10
+
+let Name="Reshma";
+let Age=20;
+let isStudent=true;
+
+// ex 11
+Name="Messi";
+Age++;
+isStudent=false;
+
+// ex 12
+let name1 = "Zane";
+let age1=30;
+let dob = "20-09-2002";
+let height=160;
+let weight="40kg";
+let degree="CSE";
+let gender="Male";
+
+
+
+
+
+
+
+
