b4c222aeddb23f9494fd15e75a27b119a4abe8e6
ReshmaJR <reshmajr1015@gmail.com>
2025-02-21 15:53:41 +0530

    Add files via upload

---
+++ Sprint2/JS/task3.js
@@ -0,0 +1,59 @@
+// ex 1
+console.log("_____EX 1_____");
+
+function sayHello(name) {
+    console.log(`Hello ${name}!`);
+
+}
+sayHello("Broo");
+
+// ex 2
+
+console.log("_____EX 2_____");
+
+function add(x, y) {
+    return x + y;
+}
+console.log("Sum of 2 nums:", add(10, 5));
+
+// ex 3
+
+console.log("_____EX 3_____");
+const multiply = function (x, y) {
+    return x * y;
+};
+console.log("5 x 3:", multiply(5, 3));
+console.log("10 x 2:", multiply(10, 2));
+console.log("7 x 4:", multiply(7, 4));
+
+// ex 4
+console.log("_____EX 4_____");
+function mult(a, b) {
+    return a * b;
+};
+console.log("5 x 8:", mult(5, 8));
+
+// ex 5
+console.log("_____EX 5_____");
+function divide(a, b) {
+    return a / b;
+};
+console.log("16/8:", divide(16, 8));
+
+// ex 6
+console.log("_____EX 6_____");
+function fact(n) {
+    return n * (n - 1);
+};
+console.log("Factorial of 5:", fact(5));
+console.log("Factorial of 10:", fact(10));
+console.log("Factorial of 12:", fact(12));
+
+// ex 7
+console.log("_____EX 7_____");
+function sqr(u) {
+    return u ** 2;
+};
+console.log("Sqr of 5:", sqr(5));
+
+
